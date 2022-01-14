0d9ffd5ea8fd7261bd544dd99721b94d16a47d50
lindabettini <linda.bettini@hotmail.it>
2022-01-14 15:15:13 +0100

    first push

---
+++ script1.js
@@ -0,0 +1,5 @@
+/*
+todo - Palidroma -
+todo .Chiedere all’utente di inserire una parola
+todo .Creare una funzione per capire se la parola inserita è palindroma
+*/
---
+++ script2.js
@@ -0,0 +1,8 @@
+/*
+todo - Pari e Dispari -
+todo .L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+todo .Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+todo .Sommiamo i due numeri
+todo .Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
+todo .Dichiariamo chi ha vinto.
+*/
