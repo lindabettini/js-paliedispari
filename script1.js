78ca2c4a2571eb5f828099fd85b45ec2b6530229
lindabettini <linda.bettini@hotmail.it>
2022-01-14 16:05:29 +0100

    Add all steps

--- script1.js
+++ script1.js
@@ -3,3 +3,28 @@ todo - Palidroma -
 todo .Chiedere all’utente di inserire una parola
 todo .Creare una funzione per capire se la parola inserita è palindroma
 */
+
+// ° Chiedo parola all'utente
+const userWord = prompt('Inserisci una porola');
+console.log(`parola inseria da utente è: ${userWord}`)
+
+// ° La giro al contrario
+let reverseUserWord = '';
+
+for (let i = userWord.length - 1; i >= 0; i--) {
+    reverseUserWord += userWord.charAt(i); 
+}
+console.log(`parola al contrario è: ${reverseUserWord}`);
+
+// ° FUNZIONE IS PALINDROMA
+function isPalindroma(word, reverseWord) {
+    if (word.value = reverseWord.value) {
+    console.log('la palora è palindroma!');
+    return true;
+    } 
+    console.log('la palora è NON palindroma!');
+    return false;
+}
+
+// ° Evoco funzione
+isPalindroma(userWord, reverseUserWord);
